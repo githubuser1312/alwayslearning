b316ed2f5b0b7f63eaa3341b55b0163fef168b66
githubuser1312 <90110389+githubuser1312@users.noreply.github.com>
2022-04-15 08:47:18 +0200

    hasta el video 57 de bbdd

--- learningpython/18bbdd.py
+++ learningpython/18bbdd.py
@@ -38,13 +38,56 @@ for producto in variosProductos: #imprimo sólo el nombre de cada artículo con 
 
 miConexion.commit()#despues de hacer un cambio en la tabla, hay que CONFIRMAR que queremos hacer ese cambio por eso se hace el commit
 
-
-
-
-
 miConexion.close()#paso 6 cerrar conexión, sólo con el paso 1 y este último se crea la base de datos aunque este vacía
 
+#OTRA BBDD
 
+miConexion2=sqlite3.connect("GestionProductos")
+miCursor2=miConexion2.cursor()
+#a continuación los campos de la tabla incluyendo PRIMARY KEY  que indica el campo clave IMPORTANTE: EL CAMPO CLAVE NO SE SE PUEDE REPETIR
+# miCursor2.execute('''
+#     CREATE TABLE PRODUCTOS2(
+#         CODIGO_ARTICULO VARCHAR(4) PRIMARY KEY,
+#         NOMBRE_ARTICULO VARCHAR(50),
+#         PRECIO INTEGER,
+#         SECCION VARCHAR(20)
+#     )
+# ''')
+
+# productos2=[
+#     ("AR01", "pelota", 10, "juguetaría"),
+#     ("AR02", "pantalón", 20, "confección"),
+#     ("AR03", "destornillador", 24, "ferretería"),
+#     ("AR04", "jarrón", 45, "cerámica")
+# ]
+# miCursor2.executemany("INSERT INTO PRODUCTOS2 VALUES (?,?,?,?)", productos2)
+
+#miCursor2.execute("INSERT INTO PRODUCTOS2 VALUES ('AR05', 'TREN', 15, 'JUGUETERIA') ") #insertamos un nuevo articulo con diferente valor de codigo articulo
+
+#si intentamos insertar un nuevo articulo con el mismo codigo key que otro de los que ya estan dentro de la bbdd, nos dara una error del tipo UNIQUE (integrityerror)
+
+#para no tener que preocuparse de insertar un cmapo clave, lo haremos asi (añadimos ID integer y autoincrement en la linea donde se incluye el primary key):
+
+miCursor2.execute('''
+    CREATE TABLE PRODUCTOS2(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE_ARTICULO VARCHAR(50),
+        PRECIO INTEGER,
+        SECCION VARCHAR(20)
+    )
+''')
+
+productos2=[
+    ("pelota", 10, "juguetaría"),
+    ("pantalón", 20, "confección"),
+    ("destornillador", 24, "ferretería"),
+    ("jarrón", 45, "cerámica")
+]
+miCursor2.executemany("INSERT INTO PRODUCTOS2 VALUES (NULL,?,?,?)", productos2)#IMPORTANTE: HAY QUE ELIMINAR UNA DE LAS 4 INTERROGACIONES QUE PUSIMOS EN EL CASO ANTERIOR Y EN SU LUGAR INTRODUCIR NULL
+
+
+miConexion2.commit()
+miConexion2.close()
 
 
 
